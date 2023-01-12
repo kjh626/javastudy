75931611c024448956296f5c6a9c0149a69b8d53
kjh626 <wjd1113@naver.com>
2023-01-12 16:04:17 +0900

    2023-01-12

---
+++ 05_inheritance/src/ex03_constructor/MainClass.java
@@ -0,0 +1,16 @@
+package ex03_constructor;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		Student student = new Student(); // Pesrson생성자가 왜 나오지? 의심
+		
+		/************************************/
+		
+		Student student2 = new Student("정숙", "가산중학교");
+		System.out.println(student2.getName());
+		System.out.println(student2.getSchool());
+	}
+
+}
