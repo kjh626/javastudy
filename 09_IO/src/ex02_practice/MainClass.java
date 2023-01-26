09310cbda8cda774d4d40a9950dd4419cad287ad
kjh626 <wjd1113@naver.com>
2023-01-26 23:37:24 +0900

    2023-01-26 home

--- 09_IO/src/ex02_practice/MainClass.java
+++ 09_IO/src/ex02_practice/MainClass.java
@@ -1,8 +1,10 @@
 package ex02_practice;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class MainClass {
 
@@ -40,11 +42,105 @@ public class MainClass {
 			}
 		}
 		
+	}
+	
+	public static void ex02() {
+		
+		File dir = new File("C:" + File.separator + "storage");
+		if(dir.exists() == false) {
+			dir.mkdirs();
+		}
+		File file = new File(dir, "ex02.txt");
+		
+		try(FileWriter fw = new FileWriter(file)) {
+			char[] cbuf = {'a','b','c','d','e'};
+			String str = "abcde";
+			
+			fw.write(cbuf, 0, 2);
+			fw.write(str, 2, 3);
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static void ex03() {
+		
+		File dir = new File("C:" + File.separator + "storage");
+		if(dir.exists() == false) {
+			dir.mkdirs();
+		}
+		File file = new File(dir, "ex03.txt");
+		
+		FileWriter fw = null;
+		BufferedWriter bw = null;
+		
+		try {
+			fw = new FileWriter(file);
+			bw = new BufferedWriter(fw);
+			
+			bw.write("Hello World");
+			
+			System.out.println("ex03.txt 파일이 생성되었다.");
+		} catch(IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(bw != null) {
+					bw.close();
+				}
+			} catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	
+	public static void ex04() {
+		
+		File dir = new File("C:" + File.separator + "storage");
+		if(dir.exists() == false) {
+			dir.mkdirs();
+		}
+		File file = new File(dir, "ex04.txt");
+		
+		try(BufferedWriter bw = new BufferedWriter(new FileWriter(file))) {
+			
+			bw.write("안녕하세요");
+			
+			System.out.println("ex04.txt 파일이 생성되었다.");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static void ex05() {
+		
+		File dir = new File("C:" + File.separator + "storage");
+		if(dir.exists() == false) {
+			dir.mkdirs();
+		}
+		File file = new File(dir, "ex05.txt");
+		
+		PrintWriter out = null;
+		
+		try {
+			out = new PrintWriter(file);
+			
+			out.println("안녕하세요.");
+			out.println("반갑습니다.");
+			
+		} catch(IOException e) {
+			e.printStackTrace();
+		} finally {
+			out.close();
+		}
 		
 	}
 	
 	public static void main(String[] args) {
-		ex01();
+		ex05();
 	}
 
 }
