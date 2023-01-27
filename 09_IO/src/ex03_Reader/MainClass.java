f7eb0ca2e29bcdf69c19d612aadfdf137bdce920
kjh626 <wjd1113@naver.com>
2023-01-27 12:48:55 +0900

    2023-01-27

--- 09_IO/src/ex03_Reader/MainClass.java
+++ 09_IO/src/ex03_Reader/MainClass.java
@@ -1,5 +1,6 @@
 package ex03_Reader;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
@@ -78,33 +79,92 @@ public class MainClass {
 		try {
 			fr = new FileReader(file);
 			char[] cbuf = new char[5];		// 5글자를 읽어 들이는 배열
+			int readCount = 0;	// 루프 시작하기 전에 선언. ☆실제로 읽은 글자의 개수☆
+			StringBuilder sb = new StringBuilder();
 			
-			while(true) {
+			while((readCount = fr.read(cbuf)) != -1) {
 				
+												// cbuf 에는 abcde, fgcde 이렇게 저장되어있다. 마치 배열처럼 저장되어있는듯.?
+				sb.append(cbuf, 0, readCount);	// 인덱스 0부터 readCount개 글자를 sb에 추가하시오.
+				
+				// **FileReader 클래스의 read() 메소드는 인자로 입력받는 버퍼에 파일의 데이터를 읽어 저장한다.
+				// **그리고 파일에서 읽은 문자의 개수를 리턴한다.
 				// ex02.txt 읽는 과정
 				//루프 ReadCount	cbuf		
 				//1		5			a b c d e
 				//2		2			f g c d e	한번 읽어들인 정보가 남아 있는 것. 모자라게 읽을 수 있다..
 				//3		-1						그래서 항상 마지막에 불필요한 데이터가 있을 확률이 높다.
 				
-				int readCount = fr.read(cbuf);	// readCount : 읽어들인 글자의 개수 // fr.read(cbuf)에서 남아있는 글자수 만큼 읽어들인다. 5글자 읽고 2글자 읽고 
+				/*
+				readCount = fr.read(cbuf);	// readCount : 읽어들인 글자의 개수 // fr.read(cbuf)에서 남아있는 글자수 만큼 읽어들인다. 5글자 읽고 2글자 읽고 
+				
 				if(readCount == -1) {
 					break;
-				}
+				}*/
 				
-				for(int i = 0; i < readCount; i++) {// readCount를 cbuf.length(5)로 바꿨을 때 쓰레기값을 볼 수 있음.
-					System.out.print(cbuf[i]);	// 2번째 읽었을때 배열에 f g c d e 이렇게 쓰레기값(c d e)이 저장되어있다. 
-				}
+				//for(int i = 0; i < readCount; i++) {// readCount를 cbuf.length(5)로 바꿨을 때 쓰레기값을 볼 수 있음.
+				//	System.out.print(cbuf[i]);	// 2번째 읽었을때 배열에 f g c d e 이렇게 쓰레기값(c d e)이 저장되어있다. 
+				//}
 					
 			}
+			
+			System.out.println(sb.toString());
+			
 		} catch(IOException e) {
 			e.printStackTrace();
+		} finally {
+			try {
+				if(fr != null) {
+					fr.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 		
 	}
 	
+	public static void ex03() {
+		
+		File dir = new File("C:" + File.separator + "storage");
+		if(dir.exists() == false) {
+			dir.mkdirs();
+		}
+		File file = new File(dir, "ex03.txt");
+		
+		BufferedReader br = null;
+		
+		try {
+			
+			br = new BufferedReader(new FileReader(file));
+			
+			char[] cbuf = new char[5];
+			int readCount = 0;
+			StringBuilder sb = new StringBuilder();
+			
+			while((readCount = br.read(cbuf)) != -1) {
+				sb.append(cbuf, 0, readCount);
+			}
+			
+			System.out.println(sb.toString());
+			
+		} catch(IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(br != null) {
+					br.close();
+				}
+			} catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	
+	
 	public static void main(String[] args) {
-		ex02();
+		ex03();
 	}
 
 }
