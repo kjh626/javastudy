e4bf2347d5f924cc63300b17b664e01680ad4e4d
kjh626 <wjd1113@naver.com>
2023-01-29 21:09:06 +0900

    2023-01-29 home

--- 05_Practice/src/practice10_Walkable/Dog.java
+++ 05_Practice/src/practice10_Walkable/Dog.java
@@ -1,5 +1,12 @@
 package practice10_Walkable;
 
-public class Dog {
+// extends Pet : 애완동물이다.
+// implements Walkable : 산책이 된다.
 
+public class Dog extends Pet implements Walkable {
+
+	public Dog(String petName) {
+		super(petName);
+	}
+	
 }
