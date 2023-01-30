104ac1f44984049e4f95aabbc6db6693333b32ec
kjh626 <wjd1113@naver.com>
2023-01-30 09:47:53 +0900

    2023-01-30

--- 09_IO/src/ex04_OutputStream/MainClass.java
+++ 09_IO/src/ex04_OutputStream/MainClass.java
@@ -3,6 +3,7 @@ package ex04_OutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class MainClass {
 
@@ -43,9 +44,47 @@ public class MainClass {
 		
 	}
 	
+	public static void ex02() {
+		// 바이트 스트림을 사용할 때 한글 데이터가 문제가 될 수 있다.
+		// => 인코딩해서 내보내는 방법을 알아야 함.
+		
+		File dir = new File("C:" + File.separator + "storage");
+		File file = new File(dir, "ex02.bin");
+		
+		FileOutputStream fos = null;
+		
+		try {
+			
+			fos = new FileOutputStream(file);
+			
+			String str = "안녕하세요";
+			
+			// getBytes(Charset charset)
+			byte[] b = str.getBytes(StandardCharsets.UTF_8);
+			
+			// getBytes(String charsetName)
+			// byte[] b = str.getBytes("UTF-8");
+			
+			fos.write(b);
+			
+		} catch(IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(fos != null) {
+					fos.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		System.out.println("ex02.bin 파일의 크기 : " + file.length() + "바이트");
+		
+	}
 	
 	public static void main(String[] args) {
-		ex01();
+		ex02();
 	}
 	
 }
