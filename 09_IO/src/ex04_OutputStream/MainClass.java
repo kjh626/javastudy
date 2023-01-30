dccbb05cc1a3aeaa959eea27b6f42137d7a03e40
kjh626 <wjd1113@naver.com>
2023-01-30 12:37:27 +0900

    2023-01-30

--- 09_IO/src/ex04_OutputStream/MainClass.java
+++ 09_IO/src/ex04_OutputStream/MainClass.java
@@ -1,9 +1,14 @@
 package ex04_OutputStream;
 
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainClass {
 
@@ -83,8 +88,125 @@ public class MainClass {
 		
 	}
 	
+	public static void ex03() {
+		
+		File dir = new File("C:" + File.separator + "storage");
+		File file = new File(dir, "ex03.bin");
+		
+		BufferedOutputStream bos = null;	// 쓸 거만 선언해라
+		
+		try {
+			
+			bos = new BufferedOutputStream(new FileOutputStream(file));
+			
+			bos.write("반갑습니다\n또만나요".getBytes("UTF-8"));	// 오타만 내지 않으면 String으로 적으면 된다.
+			
+			
+			// try의 마지막 부분에 bos.close()가 쓰일 수도 있다.(bos를 다 쓰고 난 다음에..). 현실적으로는 여기에도 많이 씀.
+		} catch(IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(bos != null) {
+					bos.close();	// 권장 위치는 여기임
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		System.out.println("ex03.bin 파일의 크기 : " + file.length() + "바이트");
+		
+	}
+	
+	public static void ex04() {
+		
+		// 변수를 그대로 출력하는 DataOutputStream
+		// 한글 처리를 할 때 자주 쓴다. String값을 주로 씀.
+		
+		File dir = new File("C:" + File.separator + "storage");
+		File file = new File(dir, "ex04.dat");
+		
+		DataOutputStream dos = null;
+		
+		try {
+			// Buffered처럼 보조 스트림이다.
+			dos = new DataOutputStream(new FileOutputStream(file));
+			
+			// 출력할 변수
+			String name = "에밀리";
+			int age = 30;
+			double height = 180.5;
+			boolean isAlive = true;
+			
+			// 출력(변수 타입에 따라서 메소드가 다름)
+			dos.writeUTF(name);
+			dos.writeInt(age);
+			dos.writeDouble(height);
+			dos.writeBoolean(isAlive);
+			
+		} catch(IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(dos != null) {
+					dos.close();
+				}
+			} catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		System.out.println("ex04.dat 파일의 크기 : " + file.length() + "바이트");
+	}
+	
+	public static void ex05() {
+		
+		// 객체를 그대로 출력하는 ObjectOutputStream
+		
+		File dir = new File("C:" + File.separator + "storage");
+		File file = new File(dir, "ex05.dat");
+		
+		ObjectOutputStream oos = null;
+				
+		try {
+			oos = new ObjectOutputStream(new FileOutputStream(file));
+		
+			// 출력할 객체
+			List<Person> people = Arrays.asList(
+				new Person("에밀리", 30, 180.5, true),
+				new Person("제시카", 35, 190.5, true)
+			);
+			
+			// 출력할 객체
+			Person person = new Person();
+			person.setName("제임스");
+			person.setAge(40);
+			person.setHeight(170.5);
+			person.setAlive(false);
+			
+			// 출력 => 직렬화가 필요하다.
+			oos.writeObject(people);
+			oos.writeObject(person);
+			
+		} catch(IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(oos != null) {
+					oos.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		System.out.println("ex05.dat 파일의 크기 : " + file.length() + "바이트");
+				
+	}
+	
 	public static void main(String[] args) {
-		ex02();
+		ex05();
 	}
 	
 }
---
+++ 09_IO/src/ex04_OutputStream/Person.java
@@ -0,0 +1,69 @@
+package ex04_OutputStream;
+
+import java.io.Serializable;
+
+/*
+	ObjectOutputStream/ObjectInputStream을 통해서
+	객체 입출력을 수행하려면 "반드시" 직렬화 처리를 해야 한다.
+*/
+/*
+	직렬화 처리 방법
+	1. java.io.Serializable 인터페이스를 구현(implements)한다. (필수)
+	2. SerialVersionUID 필드 값을 정의한다. (선택)
+*/
+public class Person implements Serializable {
+	
+	private static final long serialVersionUID = 7033755836244474051L;	// class Person에서 경고메시지 에서 generate 하면 된다.
+	// 객체 생성 위해서 준비하는 과정
+	private String name;
+	private int age;
+	private double height;
+	private boolean isAlive;
+	
+	public Person() {
+		
+	}
+
+	public Person(String name, int age, double height, boolean isAlive) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.height = height;
+		this.isAlive = isAlive;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
+
+	public boolean isAlive() {
+		return isAlive;
+	}
+
+	public void setAlive(boolean isAlive) {
+		this.isAlive = isAlive;
+	}
+	
+	
+	
+}
