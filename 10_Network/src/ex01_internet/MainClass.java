910da1a62f83a24342f1e472b8fab6111410134c
kjh626 <wjd1113@naver.com>
2023-01-31 17:17:20 +0900

    2023-01-31

--- 10_Network/src/ex01_internet/MainClass.java
+++ 10_Network/src/ex01_internet/MainClass.java
@@ -1,5 +1,7 @@
 package ex01_internet;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
@@ -265,13 +267,59 @@ public class MainClass {
 	public static void ex06_buffered() {
 		
 		String apiURL = "http://www.kma.go.kr/XML/weather/sfc_web_map.xml";
+		URL url = null;
+		HttpURLConnection con = null;
+		
+		BufferedReader reader = null;
+		BufferedWriter writer = null;
+		
+		File file = null;
+		
+		try {
+			
+			url = new URL(apiURL);
+			con = (HttpURLConnection) url.openConnection();
+			
+			String message = null;
+			int responseCode = con.getResponseCode();
+			if(responseCode == HttpURLConnection.HTTP_OK) {
+				reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+				file = new File("C:" + File.separator + "pracstorage", "sfc_web_map.xml");
+				message = "다운로드 성공";
+			} else {
+				reader = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+				file = new File("C:" + File.separator + "pracstorage", "다운로드실패.html");
+				message = "다운로드 실패";
+			}
+			
+			String line = null;
+			StringBuilder sb = new StringBuilder();
+			while((line = reader.readLine()) != null) {
+				sb.append(line + "\n");
+			}
+			
+			writer = new BufferedWriter(new FileWriter(file));
+			writer.write(sb.toString());
+			
+			reader.close();
+			writer.close();
+			con.disconnect();
+			
+			System.out.println(message);
+			
+		} catch (MalformedURLException e) {
+			System.out.println("apiURL 주소 오류");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 		
 	}
 	
 	
 	
 	public static void main(String[] args) {
-		ex06();
+		ex06_buffered();
 	}
 
 }
