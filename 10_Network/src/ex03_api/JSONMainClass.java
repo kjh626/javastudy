5895d4d9a3b8cf005b55359ea442ceca2a5ec623
kjh626 <wjd1113@naver.com>
2023-02-01 12:41:41 +0900

    2023-02-01

--- 10_Network/src/ex03_api/JSONMainClass.java
+++ 10_Network/src/ex03_api/JSONMainClass.java
@@ -1,13 +1,12 @@
 package ex03_api;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
+
+import org.json.JSONObject;
 
 
 public class JSONMainClass {
@@ -16,10 +15,10 @@ public class JSONMainClass {
 		// 요청 주소로 함, 데이터 표출 방식은 옵션이지만 반드시 json으로 해줘야 한다.
 		String serviceKey = "/crf+XWfbPub+yJFATeJmDpsQiWP4ffIHn3Fa2CFRpEwxkb+iSoDidUPYuNuoiVX7msyy1xzyz/yQ0dUbGZx6w==";
 		String apiURL = "http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty";
+						
 		URL url = null;
 		HttpURLConnection con = null;
 		BufferedReader reader = null;
-		BufferedWriter writer = null;
 		
 		try {
 			String sidoName = "서울";
@@ -57,11 +56,13 @@ public class JSONMainClass {
 			reader.close();
 			con.disconnect();
 			
-			File file = new File("C:" + File.separator + "storage", "시도별 실시간 측정정보 조회.json");
-			writer = new BufferedWriter(new FileWriter(file));
-			writer.write(sb.toString());
-			writer.close();
-			
+			System.out.println(sb.toString());
+			JSONObject obj = new JSONObject(sb.toString());
+			// value값을 가져오기 위해서 어떻게 해야할까?  // 하나씩 쭈욱 벗겨서 봤다.- {"response":{"body":{"totalCount":40,"items":[{" ... 
+			// 방법은 2가지 => (JSONObject)obj.get("response") / obj.getJSONObject("response")
+			JSONObject body = obj.getJSONObject("response").getJSONObject("body"); 		// obj3 == body 메소드 체이닝
+		
+			// 주소 + 파라미터 + 방식(GET인지) => 입력하면 응답(결과) 미리 알려주는 소프트웨어 많다. 우리는 Postman이라는 프로그램 쓸 거임. ==> 우리가 파싱하기가 편해진다. 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
