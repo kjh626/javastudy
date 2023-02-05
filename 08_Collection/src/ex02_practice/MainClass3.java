dd157b23cccb3e2ac6b9b0f95b0ab245652ea159
kjh626 <wjd1113@naver.com>
2023-02-05 23:10:45 +0900

    2023-02-05

---
+++ 08_Collection/src/ex02_practice/MainClass3.java
@@ -0,0 +1,64 @@
+package ex02_practice;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainClass3 {
+
+	public static void ex01() {
+		
+		List<String> list = new ArrayList<String>();
+		
+		list.add("summer");
+		list.add("autumn");
+		list.add("frog");
+		list.add("winter");
+		list.add(0, "spring");
+		
+		list.remove(3);
+		
+		list.set(2, "fall");
+		
+		System.out.println(list);
+		System.out.println(list.size());
+		System.out.println(list.get(3));
+	}
+	
+	public static void ex02() {
+		
+		List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+		
+		System.out.println("리스트 길이 : " + list.size());
+		
+		System.out.println("첫 번째 요소 : " + list.get(0));
+		System.out.println("마지막 요소 : " + list.get(list.size() -1));
+		
+		int total = 0;
+		for(int i = 0, length = list.size(); i < length; i++) {
+			total += list.get(i);
+		}
+		System.out.println("모든 요소의 합 : " + total);
+	}
+	
+	public static void ex03() {
+		
+		List<User2> userList = new ArrayList<User2>();
+		
+		for(int i = 0; i < 3; i++) {
+			User2 user = new User2("user", "123456");
+			userList.add(user);
+		}
+		
+		for(int i = 0, length = userList.size(); i < length; i++) {
+			System.out.println(userList.get(i).getId());
+		}
+	}
+	
+	public static void main(String[] args) {
+
+		ex03();
+		
+	}
+
+}
