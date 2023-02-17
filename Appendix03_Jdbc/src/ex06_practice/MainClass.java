3fb5c2998eebfa33626ce8eab373c1e9c0a833f4
kjh626 <wjd1113@naver.com>
2023-02-17 09:06:36 +0900

    2023-02-17

--- Appendix03_Jdbc/src/ex06_practice/MainClass.java
+++ Appendix03_Jdbc/src/ex06_practice/MainClass.java
@@ -6,6 +6,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 public class MainClass {
@@ -51,6 +53,61 @@ public class MainClass {
 				
 			}
 			System.out.println(member);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(rs != null) rs.close();
+				if(ps != null) ps.close();
+				if(con != null) con.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	
+	public static void ex02() {
+		
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		
+		try {
+			
+			Class.forName("oracle.jdbc.OracleDriver");
+			Properties p = new Properties();
+			p.load(new BufferedReader(new FileReader("db.properties")));
+			
+			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			
+			con = DriverManager.getConnection(url, p);
+			
+			String sql = "SELECT MEMBER_NO, ID, NAME, ADDRESS, JOIN_DATE";
+			sql += " FROM MEMBER_TBL";
+			
+			ps = con.prepareStatement(sql);
+			
+			rs = ps.executeQuery();
+			
+			List<Member> memberList = new ArrayList<Member>();
+			
+			while(rs.next()) {
+				
+				Member member = new Member();
+				member.setMember_no(rs.getInt(1));
+				member.setId(rs.getString(2));
+				member.setName(rs.getString(3));
+				member.setAddress(rs.getString(4));
+				member.setJoin_date(rs.getDate(5));
+			
+				memberList.add(member);
+			}
+			
+			for(int i = 0; i < memberList.size(); i++) {
+				System.out.println(memberList.get(i));
+			}
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
