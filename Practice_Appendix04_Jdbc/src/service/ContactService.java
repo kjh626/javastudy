9fef43cdc5b56c1efa9715e665adc2daad8fbb6d
kjh626 <wjd1113@naver.com>
2023-02-18 22:39:25 +0900

    2023-02-18

---
+++ Practice_Appendix04_Jdbc/src/service/ContactService.java
@@ -0,0 +1,10 @@
+package service;
+
+public interface ContactService {
+	
+	public void addcContact();
+	public void removeContact();
+	public void modifyContact();
+	public void findContactsByName();
+	public void findAllContacts();
+}
