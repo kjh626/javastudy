5e47f74e5975c34dcfb4351c03b3c4d8a3424b16
kjh626 <wjd1113@naver.com>
2023-02-18 17:29:06 +0900

    2023-02-18

--- Practice_Appendix04_Jdbc/src/dao/ContactDAO.java
+++ Practice_Appendix04_Jdbc/src/dao/ContactDAO.java
@@ -1,8 +1,12 @@
 package dao;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.Properties;
 
 public class ContactDAO {
 
@@ -22,4 +26,37 @@ public class ContactDAO {
 	
 	/***** 3. method *****/
 	
+	// 공통 메소드 - 1 (Connection 얻기)
+	private Connection getConnection() {
+		
+		Connection con = null;
+		
+		try {
+			
+			Class.forName("oralce.jdbd.OracleDriver");
+			
+			Properties p = new Properties();
+			p.load(new BufferedReader(new FileReader("db.properties")));
+			
+			con = DriverManager.getConnection(p.getProperty("url"), p);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return con;
+	}
+	
+	// 공통 메소드 - 2 (사용한 자원 반납)
+	private void close() {
+		
+		try {
+			if(rs != null) rs.close();
+			if(ps != null) ps.close();
+			if(con != null) con.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
 }
